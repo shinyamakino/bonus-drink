19ef900a3e59e2eed686d3ac7048ee7d1f8c26c4
Shinya Makino <shinya.mk77@gmail.com>
2019-02-08 18:21:58 +0900

    [mod]BonusDrinkの解答

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,20 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total_num = amount
+    empty_num = 0
+    while (amount > 0) do
+      divmod = amount.divmod(3)
+      total_num += divmod.first
+      empty_num += divmod.last
+      amount = divmod.first 
+    end
+
+    while (empty_num / 3 > 0) do
+      divmod = empty_num.divmod(3)
+      total_num += divmod.first
+      empty_num = divmod.inject(:+) 
+    end
+
+    total_num
   end
 end
